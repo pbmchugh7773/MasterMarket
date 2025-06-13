613debf4790078d8fcf5e9c797db9bc66676c506
Patrick McHugh <patrick.mchugh@kisglobal.ie>
2025-06-14 00:24:03 +0100

    elimina carpetamobile_copy

--- mobile_copy/mobile/app/(tabs)/admin.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function AdminScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 20 }}>👨‍💼 Panel de Administración</Text>
-    </View>
-  );
-}
--- mobile_copy/mobile/lib/auth.ts
+++
@@ -1,6 +0,0 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-export const isLoggedIn = async (): Promise<boolean> => {
-  const token = await AsyncStorage.getItem('token');
-  return !!token;
-};
